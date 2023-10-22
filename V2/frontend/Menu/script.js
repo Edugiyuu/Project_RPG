9b33d9a9a6cdda5fed5bff9a5c7481ab2dbc9f88
Eduardo Santos <edupaz077@gmail.com>
2023-10-22 13:37:12 -0300

    update

---
+++ V2/frontend/Menu/script.js
@@ -0,0 +1,15 @@
+const playButton = document.querySelector('#playButton')
+const credits = document.querySelector('#credits')
+const loading = document.querySelector('#loading')
+
+function LoadingAnimation() {
+    playButton.style.display = `none` 
+    credits.style.display = "none"
+    loading.style.display = 'flex'
+
+    setTimeout(function() {
+        window.location.href = '../Character/index.html';
+    }, 3000); // 2000 milissegundos (2 segundos)
+}
+
+playButton.addEventListener('click',LoadingAnimation)
